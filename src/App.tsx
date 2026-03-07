35eea20a52b7092bf6439a55692422a15fede177
nimalrathna <35356220+nimalrathna@users.noreply.github.com>
2026-03-07 18:50:06 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Calendar, Clock, MapPin, Ticket, Download, CheckCircle, ChevronRight, M
 // Ensure the attached photo is saved in your project's "public" folder as "dinali-portrait.jpg"
 const DINALI_TICKET_IMAGE_URL = "/dinali-portrait.jpg";
 // LIVE GOOGLE APPS SCRIPT URL
-const GOOGLE_API_URL = "https://script.google.com/macros/s/AKfycbyY35NOnjBIvseJyujdhyjWXRv4YQSASe303DwioifiCIDift2yaf3jtylzvMcX0xAL/exec";
+const GOOGLE_API_URL = "https://script.google.com/macros/s/AKfycbxerEaAtVzRim2alzMuwAiDJkereGDtQcO11aw8AzdxBiSDCyvPMuSnQ9YLKY113nnU/exec";
 
 export default function App() {
   const TICKET_PRICE = 40;
